15ca7e4120a606334d7102240c7231e3d3e91226
Asim K <akarowaliya@hotmail.com>
2019-09-29 14:04:52 -0500

    added player movement

---
+++ X-Machina/Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed = 10f;
+    bool facingRight = false;
+
+    public Rigidbody2D body;
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
+        body.velocity = new Vector2(move * Speed, body.velocity.y);
+        
+       
+
+        if(move > 0 && !facingRight)
+        {
+            Flip();
+        }else if(move <0 && facingRight)
+        {
+            Flip();
+        }
+
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
