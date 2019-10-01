fdc2cce3d596e09fdf1b248d4bb3751feee63d03
Asim K <akarowaliya@hotmail.com>
2019-10-01 16:10:13 -0500

    added player jump function

--- X-Machina/Assets/Scripts/Player.cs
+++ X-Machina/Assets/Scripts/Player.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float Speed = 10f;
+    public float jumpForce;
+
+    bool IsJumping;
     bool facingRight = true;
 
     public Rigidbody2D body;
 
-    Animator anim;
+    public Animator anim;
+
 
     // Start is called before the first frame update
     void Start()
@@ -26,17 +30,47 @@ public class Player : MonoBehaviour
         anim.SetFloat("Speed", Mathf.Abs(move));
 
         body.velocity = new Vector2(move * Speed, body.velocity.y);
-        
-       
 
-        if(move > 0 && !facingRight)
+            Jump();
+
+        if (move > 0 && !facingRight)
         {
+            
             Flip();
+            
         }else if(move <0 && facingRight)
         {
+           
             Flip();
-        }
 
+          
+        }
+         
+        
+    }
+
+    void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            IsJumping = true;
+            anim.SetBool("IsJumping", true);
+            body.AddForce(new Vector2(body.velocity.x, jumpForce));
+        }
+        else
+        {
+            anim.SetBool("IsJumping", false);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            IsJumping = false;
+
+            body.velocity = Vector2.zero;
+        }
     }
 
     void Flip()
