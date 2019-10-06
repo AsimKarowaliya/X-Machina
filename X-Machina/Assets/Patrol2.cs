542ed58f078048fc43d9b3378c28464307fc9c7c
Asim K <akarowaliya@hotmail.com>
2019-10-06 17:33:23 -0500

    Update Patrol2.cs

--- X-Machina/Assets/Patrol2.cs
+++ X-Machina/Assets/Patrol2.cs
@@ -35,7 +35,7 @@ public class Patrol2 : MonoBehaviour
                 timebetweenattack -= Time.deltaTime;
             }
 
-            speed = 0;
+            speed = 1;
             if (!hitinfo2.transform.CompareTag("Player"))
             {
                 speed = 1;
@@ -54,7 +54,7 @@ public class Patrol2 : MonoBehaviour
                 timebetweenattack -= Time.deltaTime;
             }
             
-            speed = 0;
+            speed = 1;
             if (!hitinfo2.transform.CompareTag("Player"))
             {
                 speed = 1;
