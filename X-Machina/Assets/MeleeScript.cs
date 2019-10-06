9ab8abebd06329dcedb79e5b9f381f5df09d4a8d
Asim K <akarowaliya@hotmail.com>
2019-10-06 17:46:21 -0500

    melee Script works

---
+++ X-Machina/Assets/MeleeScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeScript : MonoBehaviour
+{
+    public float speed;
+    public float distance;
+    private bool goingRight = true;
+    public Transform target;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D groundInfo = Physics2D.Raycast(target.position, Vector2.down, distance);
+        if (groundInfo.collider == false)
+        {
+            if (goingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                goingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                goingRight = true;
+            }
+        }
+
+        //RaycastHit2D wallinfo = Physics2D.Raycast(target.position, Vector2.left, distance);
+        //if (wallinfo.collider == true && wallinfo.transform.gameObject.tag != "Player")
+        //{
+        //    if (goingRight == true)
+        //    {
+        //        transform.eulerAngles = new Vector3(0, -180, 0);
+        //        goingRight = false;
+        //    }
+        //    else
+        //    {
+        //        transform.eulerAngles = new Vector3(0, 0, 0);
+        //        goingRight = true;
+        //    }
+        //}
+    }
+}
