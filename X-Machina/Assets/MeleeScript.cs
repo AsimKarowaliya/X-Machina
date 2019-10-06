3923a9148e36fcac43b0696e0f4a5af1d4171815
Asim K <akarowaliya@hotmail.com>
2019-10-06 17:55:03 -0500

    melee AI takes damage

--- X-Machina/Assets/MeleeScript.cs
+++ X-Machina/Assets/MeleeScript.cs
@@ -8,6 +8,8 @@ public class MeleeScript : MonoBehaviour
     public float distance;
     private bool goingRight = true;
     public Transform target;
+    public int health;
+    public GameObject deathEffect;
     // Start is called before the first frame update
 
     // Update is called once per frame
@@ -29,6 +31,11 @@ public class MeleeScript : MonoBehaviour
             }
         }
 
+        if (health <= 0)
+        {
+            Die();
+        }
+
         //RaycastHit2D wallinfo = Physics2D.Raycast(target.position, Vector2.left, distance);
         //if (wallinfo.collider == true && wallinfo.transform.gameObject.tag != "Player")
         //{
@@ -43,5 +50,34 @@ public class MeleeScript : MonoBehaviour
         //        goingRight = true;
         //    }
         //}
+    }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+    void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+    void ResetMat()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        //Debug.Log(gameObject.name);
+
+        if (coll.CompareTag("Bullet"))
+        {
+            //Instantiate(DeathEffect, transform.position, Quaternion.identity);
+            //Destroy(gameObject);
+            GetComponent<SpriteRenderer>().color = Color.red;
+            Invoke("ResetMat", 0.05f);
+            // HealthSystem SN = coll.GetComponent<HealthSystem>();
+            // SN.playerHealth -= 1;
+        }
+
     }
 }
