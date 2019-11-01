56a93f539cf868412d025540dc7746006d40591d
loklok12614 <loklok12614@gmail.com>
2019-10-31 23:29:52 -0500

    grenade controller

---
+++ X-Machina/Assets/Grenade.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+
+    float timer = 3;
+    float countdown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = timer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
