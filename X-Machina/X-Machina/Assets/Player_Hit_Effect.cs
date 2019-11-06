d0a12ece0cbbd8d169ef187a43e572c319113e04
Asim K <akarowaliya@hotmail.com>
2019-11-05 18:44:20 -0600

    fixes

---
+++ X-Machina/X-Machina/Assets/Player_Hit_Effect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Hit_Effect : MonoBehaviour
+{
+    void ResetMat()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+       
+        if (coll.CompareTag("BlueBullet") || coll.CompareTag("Enemy"))
+        {
+            //Instantiate(DeathEffect, transform.position, Quaternion.identity);
+            //Destroy(gameObject);
+            GetComponent<SpriteRenderer>().color = Color.red;
+            //this.transform.Translate(Vector2.right * 60 * Time.deltaTime);
+            Invoke("ResetMat", 0.05f);
+            // HealthSystem SN = coll.GetComponent<HealthSystem>();
+            // SN.playerHealth -= 1;
+        }
+
+    }
+}
---
+++ X-Machina/X-Machina/Assets/Weapon.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    public float distance = 0.8f;
+    public int maxGrenadeCount = 3; //maximum of grenade he can hold.
+    public int grenadeCount;
+    public GameObject grenadeMod;
+    private GameObject grenade;
+    private GameObject[] grenadeArr;
+    private int activeGrenadeCount = 0;
+    public bool grenadeActive;
+    public Animator recoileffect;
+
+    void Start()
+    {
+        grenadeCount = maxGrenadeCount;
+        grenadeActive = false;
+        grenadeArr = new GameObject[maxGrenadeCount];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && AmmoText.ammoAmount > 0)
+        {
+            Shoot();
+            recoileffect.SetBool("recoil", true);
+        }
+        else
+        {
+            recoileffect.SetBool("recoil", false);
+        }
+
+        //throwing grenade.
+        if (Input.GetKeyDown(KeyCode.G) && grenadeCount > 0)
+        {
+            //Granade();
+            GrenadeAmmo.ammoAmount -= 1;
+            //Instantiate(grenadeMod, firePoint.position, firePoint.rotation);
+            grenade = Instantiate(grenadeMod, firePoint.position, firePoint.rotation);
+            grenadeArr[activeGrenadeCount] = grenade;
+            activeGrenadeCount += 1;
+            grenadeCount -= 1;
+            grenadeActive = true;
+        }
+
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            //grenade.GetComponent<Grenade>().SuicideBombing();
+            foreach (GameObject g in grenadeArr)
+            {
+                if(g.activeSelf)
+                    g.GetComponent<Grenade>().SuicideBombing();
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        AmmoText.ammoAmount -= 1;
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+
+    void Granade()
+    {
+        GrenadeAmmo.ammoAmount -= 1;
+        Instantiate(grenadeMod, firePoint.position, firePoint.rotation);
+        grenadeCount -= 1;
+    }
+
+
+}
