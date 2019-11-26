db46be67d3e7b0f9ded5d0d6ed79bd98651ab916
loklok12614 <loklok12614@gmail.com>
2019-11-25 23:09:19 -0600

    level design

--- X-Machina/X-Machina/Assets/MeleeScript.cs
+++ X-Machina/X-Machina/Assets/MeleeScript.cs
@@ -11,8 +11,13 @@ public class MeleeScript : MonoBehaviour
     public int health;
     public GameObject deathEffect;
     private Rigidbody2D rb;
-    // Start is called before the first frame update
 
+    private Color originColor;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        originColor = GetComponent<SpriteRenderer>().color;
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -64,7 +69,7 @@ public class MeleeScript : MonoBehaviour
 
     void ResetMat()
     {
-        GetComponent<SpriteRenderer>().color = Color.white;
+        GetComponent<SpriteRenderer>().color = originColor;
     }
     void OnCollisionEnter2D(Collision2D coll)
     {
